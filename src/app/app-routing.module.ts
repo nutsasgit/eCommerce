1a4384d28022ea2951b1a3bdd5ca37bfde77dae6
Gitnuca <nino.kurtskhalia.1@gmail.com>
2025-09-19 14:25:06 +0400

    Add components: login, productpage, cart and navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { ProductPageComponent } from './features/product-page/product-page.component';
+import { CartComponent } from './features/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "logIn",
+    component: LoginComponent
+  },
+  {
+    path: "productpage",
+    component: ProductPageComponent
+  },
+  {
+    path: "cart",
+    component: CartComponent
+  }    
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { LoginComponent } from './features/login/login.component';
+import { ProductPageComponent } from './features/product-page/product-page.component';
+import { CartComponent } from './features/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    ProductPageComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
