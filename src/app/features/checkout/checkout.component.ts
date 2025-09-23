7e516d4dae6527e426f75c50a30cda38e0b919c6
Gitnuca <nino.kurtskhalia.1@gmail.com>
2025-09-23 22:01:46 +0400

    Edit checkout page.

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
+import { MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-checkout',
@@ -7,5 +12,52 @@ import { Component } from '@angular/core';
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  firstName: string = "";
+  lastName: string = "";
+  eMail: string = ""
+  address: string = "";
+  zipCode: string = ""
+
+  items: any[] = [];
+ subtotal: number = 0;
+ 
+constructor(private router: Router, private cartService: CartService){}
+
+  pay(){
+    this.firstName === "" ||
+      this.lastName === "" ||
+      this.eMail === "" ||
+      this.address === "" ||
+      this.zipCode === ""
+      {console.log("payment succesfull");
+      return;
+    }
+  }
+  ngOnInit() {
+    this.loadCart();
+  }
+loadCart() {
+    this.items = this.cartService.getItems();
+    this.subtotal = this.cartService.getTotalPrice();
+  }
+increase(item: any) {
+    item.quantity++;
+    item.total = item.quantity * item.price;
+    this.loadCart();
+  }
+decrease(item: any) {
+    if (item.quantity > 1) {
+      item.quantity--;
+      item.total = item.quantity * item.price;
+      this.loadCart();
+    }
+  }
+remove(item: any) {
+  this.cartService.removeItem(item);
+  this.loadCart();
+}
+
+
+
 
 }
