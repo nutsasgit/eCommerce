b945b49114d7c54c0d8d843df9e6a32a924bec4a
Gitnuca <nino.kurtskhalia.1@gmail.com>
2025-09-24 22:44:59 +0400

    Delete extra navbar  on a logIn page.

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,5 +10,76 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  avatar: File | null = null;
+  avatarPreview: string | ArrayBuffer | null = '/profileImage.png';
+
+  showPassword = false;
+  showConfirmPassword = false;
+  
+  constructor(private http: HttpClient, private router: Router) {}
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.avatar = file;
+
+      //Preview
+      const reader = new FileReader();
+      reader.onload = e => this.avatarPreview = reader.result;
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeAvatar() {
+    this.avatar = null;
+    this.avatarPreview = '/profileImage.png';
+  }
+
+
+  onRegister() {
+    if (this.password !== this.confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+
+    const formData: FormData = new FormData();
+    formData.append('username', this.username);
+    formData.append('email', this.email);
+    formData.append('password', this.password);
+    formData.append('password_confirmation', this.confirmPassword);
+
+    if (this.avatar instanceof File){
+      formData.append('avatar',this.avatar, this.avatar.name);
+    }
+
+    const headers = new HttpHeaders({
+      'Accept': "application/json"
+    });
+    this.http.post<{token:string}>('https://api.redseam.redberryinternship.ge/api/register',formData, {headers})
+    .subscribe({
+      next: (response) => {
+        console.log('registration successful:', response);
+        localStorage.setItem('token', response.token);
+         this.router.navigate(['/product']);
+      },
+      error:(err)=>{
+        if (err.status === 422){
+          console.error("Validation details:", err.error);
+          alert("Validation error. Check from fields")
+        } else if(err.status === 401){
+          alert("invalid Token.");
+        }
+        
+      }
+    });
+
+
+    
+  }
+
 
 }
+
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { CartService } from '../../services/cart.service';
 import { PopupComponent } from '../../features/popup/popup.component';
@@ -11,9 +13,34 @@ import { CartComponent } from '../../features/cart/cart.component';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+navbarType: number = 1;
+isLoggedIn: boolean = false;
+userAvatar: string = 'nav/Union.png';
 
 
-constructor( private dialog:MatDialog, private cartService: CartService) {}
+constructor(private router: Router, private dialog:MatDialog, private cartService: CartService) {}
+
+ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.navbarType = event.url.includes('/products') ? 2 : 1;
+      });
+
+    
+    const token = localStorage.getItem('token');
+    const avatar = localStorage.getItem('avatar');
+    if (token) {
+      this.isLoggedIn = true;
+      this.userAvatar = avatar ? avatar : 'nav/Union.png';
+    } else {
+      this.isLoggedIn = false;
+      this.userAvatar = 'nav/Union.png';
+    }
+  }
+
+
+
 Openpopup(): void {
     const items = this.cartService.getItems(); 
     if (!items || items.length === 0) {
